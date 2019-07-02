528af10c925b24bb3d1e7e127ff5d52fa8d2997f
marcopetry <marcomattospetry@gmail.com>
2019-07-02 02:23:06 -0300

    Primeiras alterações no projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Cabecalho from './componentes/Cabecalho';
 
 function App() {
   return (
-    
+    <Cabecalho />
   );
 }
 
